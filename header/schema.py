9ca2eb4ac30d371efb94064ed415865cd60d78b8
Samantha-Varguez <varaguezsamy@gmail.com>
2024-11-28 07:57:17 -0600

    update

--- header/schema.py
+++ header/schema.py
@@ -2,112 +2,90 @@ import graphene
 from graphene_django import DjangoObjectType
 from .models import Header
 from users.schema import UserType
-from django.db.models import Q
 
 class HeaderType(DjangoObjectType):
     class Meta:
         model = Header
 
 class Query(graphene.ObjectType):
-    headers = graphene.List(HeaderType, search=graphene.String())
-    headerById = graphene.Field(HeaderType, idHeader=graphene.Int())
+    # Fetch the singleton Header instance
+    header = graphene.Field(HeaderType)
 
-    def resolve_headers(self, info, search=None, **kwargs):
+    def resolve_header(self, info, **kwargs):
         user = info.context.user
         if user.is_anonymous:
             raise Exception('Not logged in!')
-        print (user)
-        if (search=="*"):
-            filter = (
-                Q(posted_by=user)
-            )
-            return Header.objects.filter(filter)[:10]
-        else:
-            filter = (
-                Q(posted_by=user) & Q(title__icontains=search)
-            )
-            return Header.objects.filter(filter)
-        
-    def resolve_headerById(self, info, idHeader, **kwargs):
-        user = info.context.user
-        if user.is_anonymous:
-            raise Exception('Not logged in!')
-        print (user)
-
-        filter = (
-            Q(posted_by=user) & Q(id = idHeader)
-        )
-        return Header.objects.filter(filter).first()
-        
-class CreateHeader(graphene.Mutation):
-    idHeader   = graphene.Int()
-    title     = graphene.String()
-    description = graphene.String()
-    posted_by = graphene.Field(UserType)
-
-    #2
-    class Arguments:
-        idHeader= graphene.Int()  
-        title     = graphene.String()
-        description = graphene.String()
-
-
-    #3
-    def mutate(self, info, idHeader, title, description):
-        user = info.context.user or None
-        if user.is_anonymous:
-            raise Exception('Not logged in !');
         print(user)
 
-        currentHeader = Header.objects.filter(id=idHeader).first()
-        print (currentHeader)
-
-        headers = Header(
-            title = title,
-            description = description,
-            posted_by  = user
-            )
+        # Fetch the singleton Header instance
+        header = Header.objects.first()
+        if not header:
+            raise Exception('Header not found!')  # Handle the absence of the Header
+        return header
         
-        if currentHeader:
-            headers.id = currentHeader.id
+class CreateOrUpdateHeader(graphene.Mutation):
+    idHeader = graphene.Int()
+    url = graphene.String()
+    title = graphene.String()
+    description = graphene.String()
+    phone = graphene.String()
+    address = graphene.String()
+    email = graphene.String()
+    socialmedia = graphene.String()
+    posted_by = graphene.Field(UserType)
 
-        headers.save()
+    # Arguments for creating or updating the header
+    class Arguments:
+        url= graphene.String()
+        title = graphene.String()
+        description = graphene.String()
+        phone = graphene.String()
+        address = graphene.String()
+        email = graphene.String()
+        socialmedia = graphene.String()
 
-        return CreateHeader(
-            idHeader  = headers.id,
-            title    = headers.title,
-            description = headers.description,
-            posted_by  = headers.posted_by
-        )
-
-class DeleteHeader(graphene.Mutation):
-    idHeader=graphene.Int()
-
-    #2 
-    class Arguments: 
-        idHeader= graphene.Int()
-
-    #3
-    def mutate(self, info, idHeader):
+    def mutate(self, info, title, description, phone, address, email, socialmedia, url):
         user = info.context.user or None
-
         if user.is_anonymous:
             raise Exception('Not logged in!')
-        print (user)
+        print(user)
 
-        currentHeader = Header.objects.filter(id=idHeader).first()
-        print (currentHeader)
+        # Fetch or create the singleton Header
+        header, created = Header.objects.get_or_create(id=1, defaults={
+            'title': title,
+            'url': url,
+            'description': description,
+            'phone' : phone,
+            'address': address,
+            'email': email,
+            'socialmedia': socialmedia,
+            'posted_by': user,
+        })
 
-        if not currentHeader:
-            raise Exception('Invalid Header id')
-        
-        currentHeader.delete()
+        if not created:  # If the Header already exists, update its fields
+            header.title = title
+            header.url = url
+            header.description = description
+            header.phone = phone
+            header.address = address
+            header.email = email
+            header.socialmedia = socialmedia
+            header.posted_by = user
+            header.save()
 
-        return DeleteHeader(
-            idHeader = idHeader,
+        return CreateOrUpdateHeader(
+            idHeader=header.id,
+            url=header.url,
+            title=header.title,
+            description=header.description,
+            phone= header.phone,
+            address=header.address,
+            email=header.email,
+            socialmedia=header.socialmedia,
+            posted_by=header.posted_by,
         )
-        
-#4
+
 class Mutation(graphene.ObjectType):
-    create_Header = CreateHeader.Field()
-    delete_Header = DeleteHeader.Field()
+    create_or_update_header = CreateOrUpdateHeader.Field()
+
+schema = graphene.Schema(mutation=Mutation)
